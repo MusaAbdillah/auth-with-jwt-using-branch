bf7086dff539ccd98518775cb580a25a9f2df245
MusaAbdillah <musa@clappingape.com>
2018-07-03 11:18:17 +0700

    finish json web token encode and decode method

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,32 @@
 module ExceptionHandler 
 	extend ActiveSupport::Concern
 
+	# define custom error subclasses, - rescue catches 'StandardErrors'
+	class AuthenticationError < StandardError; end
+	class MissingToken < StandardError; end
+	class InvalidToken < StandardError; end
+
 	included do 
+
+		# define custom handler 
+		rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+		rescue_from ExceptionHandler::AuthenticationError, with: :four_twenty_one
+		rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+		rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
 		rescue_from ActiveRecord::RecordNotFound do |e|
 			json_response({ message: e.message}, :not_found)
 		end
 
-		rescue_from ActiveRecord::RecordInvalid do |e|
-			json_response({ message: e.message}, :unprocessable_entity)
-		end
+		private 
+			# json response with message; status code 422 - unprocessable entity
+			def four_twenty_two(e) 
+				json_response({ message: e.message}, :unprocessable_entity)
+			end
+
+			# json response with message; status code  401 - unauthorized 
+			def four_twenty_one(e)
+				json_response({ message: e.message}, :unauthorized)
+			end
 	end
 end
