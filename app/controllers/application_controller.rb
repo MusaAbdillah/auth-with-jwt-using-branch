18e2ab715780836e9feabbc2176547425a827480
MusaAbdillah <musa@clappingape.com>
2018-07-03 15:21:44 +0700

    finish authentication controller and user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
 	include Response 
 	include ExceptionHandler
+
+	# callback before action 
+	before_action :authorize_request 
+
+	# attribute reader 
+	attr_reader :current_user
+
+	private 
+		def authorize_request 
+			@current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+		end
 end
